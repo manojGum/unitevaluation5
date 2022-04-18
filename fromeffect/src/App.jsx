acdb32b906fd5ab2b4f44f94a48dc39a2362de00
manojGum <Manojgum@gmail.com>
2022-04-18 16:57:26 +0530

    done
    done

---
+++ fromeffect/src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import logo from './logo.svg'
+import './App.css'
+import { From } from './components/from';
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+<From />
+    </div>
+  );
+}
+
+export default App
---
+++ fromeffect/src/components/from.jsx
@@ -0,0 +1,9 @@
+export const From=()=>{
+    return (
+        <from>
+            <input type="text" placeholder="Enter Username" />
+            <input type="number" placeholder="Enter AGe" />
+            <input type="submit" value="submit" />
+        </from>
+    )
+}
